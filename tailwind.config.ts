13a40f0181b0b060ce7d1fc145dabc5b0a94e5f6
Patrycja Milo <patrycjamilo1@gmail.com>
2024-04-22 11:27:02 +0200

    feat(theming): add color pallette for the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,5 +8,17 @@ export default {
         `plugins/**/*.{js,ts}`,
         `app.{js,ts,vue}`,
     ],
+    theme: {
+        colors: {
+            'gray': '#e2e2e2',
+            'black': '#000000',
+            'white': '#ffffff',
+            'primary': {
+                '400': '#f54363',
+                '500': '#d81f41'
+            },
+            'dark-gray': '#131313'
+        }
+    }
 }
   
