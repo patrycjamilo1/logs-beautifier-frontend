29f228c7a1f12bc30e6b345256ce90ff52841106
Patrycja Milo <patrycjamilo1@gmail.com>
2024-04-22 11:02:27 +0200

    feat(project_setup): nuxt3 basic app initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+export default {
+    darkMode: 'media',
+    content: [
+        `components/**/*.{vue,js,ts}`,
+        `layouts/**/*.vue`,
+        `pages/**/*.vue`,
+        `composables/**/*.{js,ts}`,
+        `plugins/**/*.{js,ts}`,
+        `app.{js,ts,vue}`,
+    ],
+}
+  
